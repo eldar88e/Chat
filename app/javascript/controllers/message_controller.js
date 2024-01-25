7a073985efec41047edffc83893a2e7b1e67525f
Eldar Eminov <em88el@gmail.com>
2024-01-25 04:23:07 +0300

    Update the scroll_controller.js

--- app/javascript/controllers/message_controller.js
+++ app/javascript/controllers/message_controller.js
@@ -3,9 +3,6 @@ import { Controller } from "@hotwired/stimulus"
 export default class extends Controller {
     static targets = ["output"];
 
-    connect() {
-    }
-
     clear() {
         document.addEventListener("turbo:submit-end", (event) => {
             if (event.detail.success) {
--- app/javascript/controllers/scroll_controller.js
+++ app/javascript/controllers/scroll_controller.js
@@ -8,5 +8,14 @@ export default class extends Controller {
 
     scrollToBottom() {
         this.element.scrollTop = this.element.scrollHeight;
+
+        const mainUser = document.querySelector('div.user span').textContent;
+        const messages = document.getElementById('messages').children;
+        const lastMessage = messages[messages.length - 1];
+        const lastUser = lastMessage.querySelector('div.sender').textContent.trim();
+
+        if(lastUser !== mainUser) {
+            lastMessage.classList.remove('sent');
+        }
     }
 }
