d00cd68a36fd351d343d69201889683034c2d742
Eldar Eminov <em88el@gmail.com>
2024-02-14 18:19:41 +0300

    Fix the rooms test

--- spec/controllers/rooms_controller_spec.rb
+++ spec/controllers/rooms_controller_spec.rb
@@ -7,26 +7,31 @@ RSpec.describe RoomsController, type: :controller do
 
       before { sign_in user }
 
-      it 'creates a new room with a membership for the current user' do
-        post :create, params: { name: 'Test Room' }
+      context 'creates a new room with a membership for the current user' do
+        before { post :create, params: { name: 'Test Room' } }
 
-        expect(response).to have_http_status(:ok)
-        expect(Room.count).to eq(1)
-        expect(Membership.count).to eq(1)
+        it 'returns status 200 or ok' do
+          expect(response).to have_http_status(:ok)
+        end
 
-        created_room       = Room.first
-        created_membership = Membership.first
+        it 'check count added room and membership' do
+          expect(Room.count).to eq(1)
+          expect(Membership.count).to eq(1)
+        end
 
-        expect(created_room.name).to eq('Test Room')
-        expect(created_membership.user).to eq(user)
-        expect(created_membership.room).to eq(created_room)
+        it 'check added room and membership' do
+          created_room       = Room.first
+          created_membership = Membership.first
+          expect(created_room.name).to eq('Test Room')
+          expect(created_membership.user).to eq(user)
+          expect(created_membership.room).to eq(created_room)
+        end
       end
 
       it 'redirects to root_path if room save fails' do
         allow_any_instance_of(Room).to receive(:save).and_return(false)
-
         post :create, params: { name: 'Test Room' }
-
+        expect(response).to have_http_status(302)
         expect(response).to redirect_to(root_path)
       end
     end
@@ -35,13 +40,11 @@ RSpec.describe RoomsController, type: :controller do
       context 'creates a new room for the user' do
         it 'redirects to sign_in path' do
           post :create, params: { name: 'Test Room' }
-
           expect(response).to redirect_to(new_user_session_path)
         end
 
         it 'returns a redirect status' do
           post :create, params: { name: 'Test Room' }
-
           expect(response).to have_http_status(302)
         end
       end
